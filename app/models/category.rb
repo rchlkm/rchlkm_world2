38c2f340422bdf48900bc39c1072a9f9b186e67f
Vicky Ye <vy@andrew.cmu.edu>
2019-02-22 14:32:16 -0500

    set up category model

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,7 +1,12 @@
 class Category < ApplicationRecord
 	has_many :photos
+	has_many :articles
+
+	validates :title, :presence => true
+	validates :content, :presence => true
+
 
 	scope :active, 			 -> { where('active = ?', true) }
-  scope :alphabetical, -> { order('name') }
+  	scope :alphabetical, -> { order('name') }
   
 end
